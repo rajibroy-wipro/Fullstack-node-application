017be73e146d59bc2ef18a3cc0e92e54a2cfef65
arvind <arvind.bisht.90475@gmail.com>
2020-08-14 13:05:19 +1000

    added volleyball for logging incoming req and outgoinhng response

--- app.js
+++ app.js
@@ -1,58 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const methodOverride = require('method-override');
-const flash = require('connect-flash');
-const Campground = require('./models/campground');
-const Comment = require('./models/comment');
-const User  = require('./models/user');
-const seedDB = require('./seeds');
-
+const volleyball = require("volleyball");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const Campground = require("./models/campground");
+const Comment = require("./models/comment");
+const User = require("./models/user");
+const seedDB = require("./seeds");
 
 // requiring routes
-const commentRoutes = require('./routes/comments');
-const campgroundRoutes = require('./routes/campgrounds');
-const indexRoutes = require('./routes/index');
-const url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp" ;
-
+const commentRoutes = require("./routes/comments");
+const campgroundRoutes = require("./routes/campgrounds");
+const indexRoutes = require("./routes/index");
+const url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
 
 // mongoose config for warning
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
 //mongodb+srv://arvind:22@Arvind@yelpcamp-1vbii.mongodb.net/yelpcamp?retryWrites=true&w=majority
-mongoose.connect(url, {
-	useNewUrlParser: true,
-	useCreateIndex: true
-}).then(() => {
-	console.log('Connected to DB');
-}).catch(err => {
-	console.log('ERROR:', err.message);
-});
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("ERROR:", err.message);
+  });
 
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(methodOverride('_method'));
+app.use(volleyball);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 app.use(flash());
 
 //seed the db
 //seedDB();
 
-
 // PASSPORT CONFIG
-app.use(require('express-session')({
-    secret: 'Tater is the cutest dog!',
+app.use(
+  require("express-session")({
+    secret: "Tater is the cutest dog!",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -60,25 +62,19 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-
 // middlewre for handling login logout links logic in header
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash('error');
-    res.locals.success = req.flash('success');
-    next();
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
-
 app.use(indexRoutes);
-app.use('/campgrounds',campgroundRoutes);
-app.use('/campgrounds/:id/comments',commentRoutes);
-
-
-
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,() => {
-    console.log(`yelpcamp server has started listening at port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`yelpcamp server has started listening at port ${PORT}`);
 });
